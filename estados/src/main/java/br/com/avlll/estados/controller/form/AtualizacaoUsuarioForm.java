78babb78b70921096dd1ea06b72e8315b051c9f2
Johnny <Johnnylsrdarocha@gmail.com>
2022-09-22 00:17:45 -0300

    Adicionado o código na classe, para que funcionasse o put

--- estados/src/main/java/br/com/avlll/estados/controller/form/AtualizacaoUsuarioForm.java
+++ estados/src/main/java/br/com/avlll/estados/controller/form/AtualizacaoUsuarioForm.java
@@ -3,8 +3,51 @@ package br.com.avlll.estados.controller.form;
 import br.com.avlll.estados.model.Usuario;
 import br.com.avlll.estados.respository.UsuarioRepository;
 
+
 public class AtualizacaoUsuarioForm {
+
+    private Long id;
+    private String nome;
+    private String email;
+    private String senha;
+
     public Usuario atualizar(Long id, UsuarioRepository usuarioRepository) {
-        return new Usuario();
+        Usuario usuario = usuarioRepository.getReferenceById(id);
+        usuario.setNome(nome);
+        usuario.setEmail(email);
+        usuario.setSenha(senha);
+        return usuario;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
     }
 }
